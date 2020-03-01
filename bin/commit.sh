5113bcba76487f48c863c2f5f16bf13a2c379d05
Thura Aung <thura.ucsy@gmail.com>
2020-03-01 23:17:34 +0900

    commit.sh မှာ git add လုပ်တာ v1 folder ကျန်ခဲ့

--- bin/commit.sh
+++ bin/commit.sh
@@ -7,7 +7,7 @@ git branch
 
 git config user.email "${GITHUB_EMAIL}"
 git config user.name "${GITHUB_USERNAME}"
-git add build/month/ build/year/
+git add build/v1/month/ build/v1/year/
 git commit -m "[skip travis] travis ci မှတစ်ဆင့် build လုပ်၍ရလာသော build/ အောက်ရှိ month/ year/ ကို အလိုလျောက် master branch ဆီ တစ်ရက်တစ်ခါ commit လုပ်ပေးခြင်း"
 
 git push "https://tanintharyi:${GITHUB_TOKEN}@${GITHUB_REFERENCE}" HEAD:master
