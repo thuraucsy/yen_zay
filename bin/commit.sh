5a0e3f510115d29be2f94e6fad9ff330a26e745f
Thura Aung <thura.ucsy@gmail.com>
2020-03-01 23:12:37 +0900

    - api v1 အောက်မှာ year month တွေပါ အလိုလျောက် အလုပ်လုပ်စေရန် ပြင်ဆင်ရေးသားထား - 2020/02 json file များကိုလည်း local မှာ run ပြီးထည့်သွင်းခဲ့

---
+++ bin/commit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+git status
+git branch
+
+git config user.email "${GITHUB_EMAIL}"
+git config user.name "${GITHUB_USERNAME}"
+git add build/month/ build/year/
+git commit -m "[skip travis] travis ci မှတစ်ဆင့် build လုပ်၍ရလာသော build/ အောက်ရှိ month/ year/ ကို အလိုလျောက် master branch ဆီ တစ်ရက်တစ်ခါ commit လုပ်ပေးခြင်း"
+
+git push "https://tanintharyi:${GITHUB_TOKEN}@${GITHUB_REFERENCE}" HEAD:master
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const apigClientFactory = require('aws-api-gateway-client').default;
 
 function main() {
-	console.log("main start");
+    console.log("main start");
 
     config = {
         invokeUrl: process.env.API_GATEWAY_INVOKE_URL,
@@ -13,8 +13,8 @@ function main() {
     var apigClient = apigClientFactory.newClient(config);
 
     apigClient.invokeApi({}, 'today', 'GET', {}, {})
-    .then(function(result){
-        console.log(result.data);
+    .then(function(todayResult){
+        console.log(`todayResult`, todayResult.data);
 
         var apiLocalFolder = 'build/v1/';
         var apiFolderObj = {
@@ -32,7 +32,26 @@ function main() {
             }
         }
 
-        fs.writeFileSync(`${apiFolderObj.dayFolder}today.json`, JSON.stringify(result.data, null, 4));
+        fs.writeFileSync(`${apiFolderObj.dayFolder}today.json`, JSON.stringify(todayResult.data, null, 4));
+
+        /* တကယ်လို့ today json ရလဒ်က တစ်ခုပဲရှိတယ်ဆိုရင် အဲ့နေ့ရဲ့ ပထမဆုံး update ဖြစ်တာမလို့ မနေ့က လနဲ့နှစ်ကို update လုပ်ပေးရန် */
+        if (todayResult.data.Items.length == 1) {
+            apigClient.invokeApi({}, 'year_month', 'GET', {}, {})
+            .then(function(yearMonthResult){
+                var yearMonthResObj = yearMonthResult.data;
+                console.log(`yearMonthResult`, JSON.stringify(yearMonthResObj, null, 4));
+
+                if (Object.keys(yearMonthResObj).length > 0) {
+                    fs.writeFileSync(`${apiFolderObj.yearFolder}${Object.keys(yearMonthResObj)[0].substr(0, 4)}.json`, JSON.stringify(yearMonthResObj, null, 4));
+
+                    for (var yearMonthKey in yearMonthResObj) {
+                        fs.writeFileSync(`${apiFolderObj.monthFolder}${yearMonthKey}.json`, JSON.stringify(yearMonthResObj[yearMonthKey], null, 4));
+                    }
+                }
+            }).catch( function(err){
+                console.log(err);
+            });
+        }
     }).catch( function(err){
         console.log(err);
     });
